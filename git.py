261ccd741271129a5253937b09a6d556933abe25
Encaciel <encaciel@gmail.com>
2025-10-25 10:22:05 -0300

    aulagit1

--- git.py
+++ git.py
@@ -1,11 +1,20 @@
 #git init    Em terminal, inicializa ou reinicializa o git
 #git status  Em terminal, informa se há atualizações do local para o remoto
-#git status --list  Em terminal, lista todas configurações do local
 
 #git config user.name "username"    Em terminal, determina usuário do GitHub
 #git config user.email "email"      Em terminal, determina email do GitHub
+#git config --list  Em terminal, lista todas configurações do local
 
 #git add .              Em terminal, adiciona todos arquivos do local ao remoto
 #git add "index.py"     Em terminal, adiciona um arquivo do local ao remoto
 #git commit -m "aulagit1"   Em terminal, prepara arquivos adicionados do local ao remoto
 #git branch -M main         Em terminal, cria ramo para versões do commit
+#git remote add origin <URL>   Em terminal, informa endereço do remoto, URL https://github.com/Encaciel/Hospital.git
+#git push -u origin main    Em terminal, envia arquivos do local ao remoto
+
+#PROBLEMAS!!!
+#git pull origin branchname --allow-unrelated-histories
+
+#git config --global --unset user.name  Em terminal, remover conta do local
+#git config --global --unset user.email
+#git config --global --list
